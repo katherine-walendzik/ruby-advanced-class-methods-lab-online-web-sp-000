384368e71edc3fe84abfd63ec56b4cd45925c872
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-05-05 00:44:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,11 +34,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if self.find_by_name
-      song
-    else
-      self.create_by_name
-    end
+    self.find_by_name || self.create_by_name
   end
  
 end
