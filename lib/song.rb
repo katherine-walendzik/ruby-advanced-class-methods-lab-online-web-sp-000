ee132a916037d302c7b5a1996e657b29ae0e9298
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-05-05 01:13:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,12 +46,16 @@ class Song
     song = self.new
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
-    song.save
     song
   end
   
   def self.create_from_filename
-    
+    split_filename = filename.chomp(".mp3").split(" - ")
+    song = self.new
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song.save
+    song
   end
  
 end
